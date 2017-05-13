85d5ea882a00890e73e3053dcb9a23b045ac2c93
salamander-mh <1906747819@qq.com>
2017-05-13 11:54:13 +0800

    添加ChatServer项目，用C#实现

---
+++ ChatServer/frmMain.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace 自做网络通讯
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnServerConn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnServerConn
+            // 
+            this.btnServerConn.Location = new System.Drawing.Point(306, 72);
+            this.btnServerConn.Name = "btnServerConn";
+            this.btnServerConn.Size = new System.Drawing.Size(64, 21);
+            this.btnServerConn.TabIndex = 0;
+            this.btnServerConn.Text = "启动服务";
+            this.btnServerConn.UseVisualStyleBackColor = true;
+            this.btnServerConn.Click += new System.EventHandler(this.btnServerConn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP地址：";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(103, 72);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(166, 21);
+            this.txtIP.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(103, 123);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(166, 21);
+            this.txtPort.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "端口：";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(103, 176);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(166, 100);
+            this.txtMsg.TabIndex = 5;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(306, 320);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(64, 21);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(103, 320);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(166, 21);
+            this.txtSend.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 378);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnServerConn);
+            this.Name = "Form1";
+            this.Text = "服务器端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServerConn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+    }
+}
+
---
+++ ChatServer/frmMain.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace 自做网络通讯
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        Socket socketwatch = null;
+        Thread watchthread = null;
+
+        private void btnServerConn_Click(object sender, EventArgs e)
+        {
+            socketwatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = IPAddress.Parse(txtIP.Text);
+            int port = Int32.Parse(txtPort.Text);
+            IPEndPoint endpoint =new IPEndPoint(ipaddress,port);
+            //监听绑定一个的节点
+            socketwatch.Bind(endpoint);
+            //将套接字的监听队列长度限制为20
+            socketwatch.Listen(20);
+            txtMsg.AppendText("开始监听客户端"+"\r\n");
+            watchthread = new Thread(new ThreadStart(looplisten));
+            //设置为后台线程
+            watchthread.IsBackground = true;
+            //启动线程
+            watchthread.Start();
+        }
+
+
+        //创建一个负责和客户端通信的套接字 
+        Socket socConnection = null;
+
+        private void looplisten()
+        {
+            while (true)
+            {
+                //没有收到请求就阻塞了
+                socConnection = socketwatch.Accept();
+                txtMsg.AppendText("连接客户端成功！\r\n");
+                ParameterizedThreadStart pts = new ParameterizedThreadStart(ServerRecMsg);
+                Thread th = new Thread(pts);
+                th.Start(socConnection);
+                th.IsBackground = true;
+            }
+        }
+
+        private void ServerRecMsg(object socketClientPara)
+        {
+            Socket socketServer = socketClientPara as Socket;
+            while (true)
+            {
+                //创建一个内存缓冲区 其大小为1024*1024字节  即1M
+                byte[] arrServerRecMsg = new byte[1024 * 1024];
+                //将接收到的信息存入到内存缓冲区,并返回其字节数组的长度
+                int length = socketServer.Receive(arrServerRecMsg);
+                //将机器接受到的字节数组转换为人可以读懂的字符串
+                string strSRecMsg = Encoding.UTF8.GetString(arrServerRecMsg, 0, length);
+                //将发送的字符串信息附加到文本框txtMsg上  
+                txtMsg.AppendText(GetCurrentTime() + "\r\n" + "客户端:" + "\r\n" + strSRecMsg + "\r\n");
+            }
+        }
+
+
+        //发送信息的方法
+        private void SeverSendMsg(string Msg)
+        {
+            //转化成机器能够理解的字节数组
+            byte[] arrSendMsg = Encoding.UTF8.GetBytes(Msg);
+            //发送
+            socConnection.Send(arrSendMsg);
+            txtMsg.AppendText(GetCurrentTime()+"\r\n"+"服务器端："+Msg+"\r\n");
+        }
+       
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtSend.Text=="")
+            {
+                MessageBox.Show("发送内容不能为空！");
+            } 
+            else
+            {
+                SeverSendMsg(txtSend.Text.Trim());
+                txtSend.Text = "";
+            }
+        }
+
+
+        /// <summary>
+        /// 获取当前系统时间的方法
+        /// </summary>
+        /// <returns>当前时间</returns>
+        private DateTime GetCurrentTime()
+        {
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            return currentTime;
+        }
+    }
+}
